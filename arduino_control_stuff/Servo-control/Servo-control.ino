6fa51c98bcfe62ccfd698bbc4a86d5158ed07982
Kiet Le <ktlbusiness11@gmail.com>
2024-10-18 18:55:47 -0400

    Add serial logs

--- arduino_control_stuff/Servo-control/Servo-control.ino
+++ arduino_control_stuff/Servo-control/Servo-control.ino
@@ -1,12 +1,3 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
-*/
-
 #include <Servo.h>
 
 Servo myservo;  // create Servo object to control a servo
@@ -17,6 +8,7 @@ int pos = 0;    // variable to store the servo position
 int piSignalPin = 3;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(piSignalPin, INPUT);
   myservo.attach(2);  // attaches the servo on pin 9 to the Servo object
 }
