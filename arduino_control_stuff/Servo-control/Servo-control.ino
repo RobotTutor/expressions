7d68b3cfee42c643c303dd6d99159bc160281d72
Leroy Musa <musaleroyohio@gmail.com>
2024-10-18 19:19:07 -0400

    Modify Ears movement

--- arduino_control_stuff/Servo-control/Servo-control.ino
+++ arduino_control_stuff/Servo-control/Servo-control.ino
@@ -17,14 +17,14 @@ void loop() {
   int piSignal = digitalRead(piSignalPin);
 
   if (piSignal == HIGH) {
-      for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      for (pos = 0; pos <= 130; pos += 1) { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
-        delay(16);                       // waits 15 ms for the servo to reach the position
+        delay(8);                       // waits 15 ms for the servo to reach the position
       }
-      for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      for (pos = 130; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
-        delay(16);                       // waits 15 ms for the servo to reach the position
+        delay(8);                       // waits 15 ms for the servo to reach the position
       }
   } else {
     Serial.println("Not rotating");
